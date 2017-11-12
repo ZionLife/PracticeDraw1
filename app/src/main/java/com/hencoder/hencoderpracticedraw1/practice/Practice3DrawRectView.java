df66dcdf4557529efede3dbd80fa8ff538d86a58
Zion <ZionLife1025@163.com>
2017-11-12 15:00:51 +0800

    <Update>完成Practice3

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
@@ -2,6 +2,9 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,5 +28,12 @@ public class Practice3DrawRectView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawRect() 方法画矩形
+
+
+        Rect rect = new Rect(50, 50, 100, 100);
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(rect, paint);
     }
 }
