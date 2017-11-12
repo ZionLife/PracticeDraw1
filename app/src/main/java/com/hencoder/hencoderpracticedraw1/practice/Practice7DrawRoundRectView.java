4b9a4599a11b4eff013f4935b78b67f1e8d6c773
Zion <ZionLife1025@163.com>
2017-11-12 15:51:27 +0800

    <Update>完成Practice7

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice7DrawRoundRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice7DrawRoundRectView.java
@@ -2,6 +2,9 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,5 +28,12 @@ public class Practice7DrawRoundRectView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawRoundRect() 方法画圆角矩形
+
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        int dx = getWidth() / 4;
+        int dy = getHeight() / 4;
+        RectF rectf = new RectF(dx * 1, dy * 1, dx * 3, dy * 3);
+        canvas.drawRoundRect(rectf, 30, 30, paint);
     }
 }
