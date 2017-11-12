eb67d1cc6a7ad95b5d48a29544e37b01340b3644
Zion <ZionLife1025@163.com>
2017-11-12 15:35:08 +0800

    <Update>完成Practice6

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice6DrawLineView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice6DrawLineView.java
@@ -2,6 +2,7 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,5 +26,15 @@ public class Practice6DrawLineView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawLine() 方法画直线
+
+        float width = getWidth();
+        float height = getHeight();
+        float dx = width / 3; // 宽度的三分一
+        float dy = height / 3; //高度的三分一
+
+
+        Paint paint = new Paint();
+        paint.setStrokeWidth(5);
+        canvas.drawLine(dx * 1, dy * 1, dx * 2, dy * 2, paint);
     }
 }
